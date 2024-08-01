868b2b42d247a039b702bdfbe4bc7749688eb6e5
harishikesh1 <harishikesh1@gmail.com>
2024-08-01 19:00:30 +0530

    Committing changes before running codemod

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /node_modules\/@mapbox\/node-pre-gyp\/lib\/util\/nw-pre-gyp\/index\.html$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
+  transpilePackages: ["@repo/ui", "@repo/db"],
 };
+
+module.exports = nextConfig;
